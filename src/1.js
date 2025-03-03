d9847305a15581eae861f7d875726bd45183065e
k4ashimo <k4ashimo@gmx.com>
2025-03-03 13:05:10 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,19 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return null;
+    } else {
+        return a / b;
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
