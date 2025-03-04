361af132f2f42ff898c33da2692b5501cf0b4fbc
k4ashimo <k4ashimo@gmx.com>
2025-03-04 13:49:28 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,18 @@
+function calculate(num1, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+  }
+  return result;
+}
